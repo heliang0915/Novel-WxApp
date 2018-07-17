8a933040d47953c5b705a1d346f4b00531ddbbea
heliang <heliang@duyao001.com>
2018-07-17 20:20:02 +0800

    准备添加分类页

--- pages/book/category/index/index.js
+++ pages/book/category/index/index.js
@@ -4,7 +4,14 @@ let  app = getApp()
 let fetch=require("../../../../utils/fetch.js");
 Page({
   data: {
-
+    padding:32,
+    pixelRatio:2,
+    screenHeight:100,
+    category: {},
+    map: {
+      male: "男生",
+      female: "女生"
+    }
   },
   //分享
   onShareAppMessage: function () {
@@ -18,6 +25,33 @@ Page({
     let self = this;
     fetch.get("api/categories",true).then((data) => {
       console.log(data);
+      delete data['ok'];
+      delete data['picture'];
+      delete data['press'];
+      for(let key in data){
+        let ary=data[key];
+        ary.forEach((item)=>{
+            item.name=item.name == "古代言情" ? "古言" : item.name;
+            item.name = item.name == "现代言情" ? "现言" : item.name;
+            item.name = item.name == "青春校园" ? "校园" : item.name;
+            item.name = item.name == "玄幻奇幻" ? "奇幻" : item.name;
+            item.name = item.name == "武侠仙侠" ? "仙侠" : item.name;
+            item.name = item.name == "游戏竞技" ? "游戏" : item.name;
+            item.name = item.name == "悬疑灵异" ? "灵异" : item.name;
+
+            let bookCover = item.bookCover;
+            let newAry=[];
+            bookCover.forEach((cover) => {
+              cover = decodeURIComponent(cover.replace('/agent/', ''));
+              console.log(cover);
+              newAry.push(cover);
+            })
+            item.bookCover = newAry;
+        })
+      }
+      self.setData({
+        category: data
+      })
     }).catch((err) => {
       console.log(err);
     })
@@ -26,7 +60,17 @@ Page({
     wx.setNavigationBarTitle({
       title:"分类"
     })
-
+    let sefl=this;
+    wx.getSystemInfo({
+      success: function (res) {
+        let { pixelRatio, screenHeight}=res;
+        sefl.setData({
+          pixelRatio,
+          padding: pixelRatio==2?30:34,
+          screenHeight
+        })
+      }
+    })
     this.getCategoriesList();
   }
 })
